0da50395761b35c04d10633f28839dfe66239534
imnavanath <navanathb@bsf.io>
2020-05-05 11:52:06 +0530

    Added previously deleted options

--- inc/core/class-astra-theme-options.php
+++ inc/core/class-astra-theme-options.php
@@ -203,6 +203,7 @@ if ( ! class_exists( 'Astra_Theme_Options' ) ) {
 					// Header - Primary.
 					'disable-primary-nav'                 => false,
 					'header-layouts'                      => 'header-main-layout-1',
+					'header-main-rt-section'              => 'none',
 					'header-display-outside-menu'         => false,
 					'header-main-rt-section-html'         => '<button>' . __( 'Contact Us', 'astra' ) . '</button>',
 					'header-main-rt-section-button-text'  => __( 'Button', 'astra' ),
